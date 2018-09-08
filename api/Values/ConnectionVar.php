1dedaef3e68aa41704e190e415342e8c6e74374b
Lorena <lorena.aires.92@gmail.com>
2018-09-08 18:41:16 +0200

    Prossimi concerti responsive

---
+++ api/Values/ConnectionVar.php
@@ -0,0 +1,12 @@
+<?php 
+    //$host="MariaDB";
+    //$user = "Sql1049322";
+    //$password="f76075174z";
+    //$dbname = "Sql1049322_2";
+    //$host="localhost";    $user = "Sql1049322";    $password="f76075174z";    $dbname = "Sql1049322_2";
+    $host="localhost";
+    $user = "root";
+    $password="";
+    $dbname = "movedb";
+
+?>
